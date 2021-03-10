b65d8295214d654e73497ff5824d93e9f2c81953
Raul <rauldavila323@gmail.com>
2021-03-10 19:45:50 +0100

    Server has been connected and API key has been hidden.

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const https = require("https")
+require('dotenv').config();
+
+console.log(process.env)
 
 const app = express();
 
+const api_key = process.env.API_Key
+
 app.get('/', function(request ,response){
 
-	const weatherGet = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=cb229d7e6e74cb04fa48087611af7973&units=imperial";
+	const weatherGet = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${api_key}&units=imperial`;
 
 	https.get(weatherGet, function(res){
 		console.log(res)
